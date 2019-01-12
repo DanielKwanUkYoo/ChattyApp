78c8ed6e46b4dbdcebf66ec49f4060a084f70723
DanielKwanUkYoo <kwanukyoo0722@gmail.com>
2019-01-12 01:09:01 +0000

    cleaned up code (broadcast) avoiding DRY

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -3,31 +3,28 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const PORT = 3001;
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
-
 const wss = new SocketServer({ server });
-
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
 
 wss.on('connection', (ws) => {
   console.log('Client connected!!!!');
 //-------------------------------------------  
-  
+  wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === ws.OPEN) {
+        client.send(data);
+      }
+    });
+  };
+
   const numberOfClients = {
     numOfUser: wss.clients.size,
     type: "Incoming newUser"
   };
 
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === ws.OPEN) {
-      client.send(JSON.stringify(numberOfClients));
-    }
-  });    
-  // ws.send(JSON.stringify(numberOfClients));
+  wss.broadcast(JSON.stringify(numberOfClients));    
+
 //--------------------------------------------    
   ws.on('message', function incoming(data){
 
@@ -44,24 +41,16 @@ wss.on('connection', (ws) => {
       break;
     }; 
 
-      
-      wss.clients.forEach(function each(client) {
-          if (client.readyState === ws.OPEN) {
-            client.send(JSON.stringify(parsedData));
-          }
-      });    
-    })
+    wss.broadcast(JSON.stringify(parsedData));
+  
+  })
 //-------------------------------------------
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+
   ws.on('close', () => {
     const numberOfClientsLeft = {
       numOfUser: wss.clients.size,
       type: "Incoming newUser"
     };
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === ws.OPEN) {
-        client.send(JSON.stringify(numberOfClientsLeft));
-      }
-    });
+    wss.broadcast(JSON.stringify(numberOfClientsLeft))
   })
 })
